2dabeee41b1e675bcd3a7413d47a5e28b13c3a3c
arrowToTarget <1017635978@qq.com>
2016-11-07 13:33:18 +0800

    Master-002 complish static resource config

--- master/src/main/java/com/lewis/master/controller/HelloController.java
+++ master/src/main/java/com/lewis/master/controller/HelloController.java
@@ -2,7 +2,6 @@ package com.lewis.master.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * Created by Administrator on 2016/11/3.
@@ -12,9 +11,8 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class HelloController {
 
     @RequestMapping("/say")
-    @ResponseBody
     public String sayHello(String name){
         System.out.println("got here");
-        return "hello "+name;
+        return "Hello";
     }
 }
---
+++ master/src/main/webapp/WEB-INF/views/Hello.jsp
@@ -0,0 +1,12 @@
+
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+    <script src="../static/js/jquery.min.js" type="text/javascript"></script>
+</head>
+<body>
+<h1>Hello ! everyone !</h1>
+<h2>HAHAHAA!</h2>
+</body>
+</html>
