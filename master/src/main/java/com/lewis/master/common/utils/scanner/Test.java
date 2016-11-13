f9ae1b658b5e6703d8fd71b0356dc65356ae615d
arrowToTarget <1017635978@qq.com>
2016-11-13 23:21:51 +0800

    Master-007 complish scanner package test

---
+++ master/src/main/java/com/lewis/master/common/utils/scanner/Test.java
@@ -0,0 +1,25 @@
+package com.lewis.master.common.utils.scanner;
+
+import com.lewis.master.common.utils.ListUtil;
+
+import java.util.List;
+
+/**
+ * Created by Administrator on 2016/11/13.
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        List<Class> classes = ScannerClassUtil.scanPackage("com.lewis.master", new ClassFilter() {
+            public boolean accept(Class clazz) {
+                return clazz.getName().endsWith("Util");
+            }
+        });
+        if (ListUtil.isNotEmpty(classes)) {
+            for (Class aClass : classes) {
+                System.out.println(aClass.getName());
+            }
+        }
+
+    }
+}
